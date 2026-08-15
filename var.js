6a21e889e170a87db4d722e818482b5f65d79509
Mahati <169645148+mahati-progress@users.noreply.github.com>
2026-08-15 20:00:51 +0530

    first commit

---
+++ var.js
@@ -0,0 +1,32 @@
+var x = 10;
+function test() {
+  var y = 20;
+
+}
+console.log(x);
+
+// issue with var
+
+var flag ="hey";
+var t1 = 4;
+
+if (t1 > 3) {
+  var flag = "hey tom";
+}
+
+console.log(flag);
+
+
+//let
+
+let m = "hey pal";
+let time = 4;
+if (time > 3) {
+  let msg = "hey how are you";
+  console.log(msg);
+}
+
+console.log(msg);
+console.log(m);
+
+
